e5d0ecc8783922b3fd1a6df09a50e400610186bf
swetha-A130 <swethachintalapudi6@gmail.com>
2024-11-08 14:53:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let modal = document.querySelector('.modal');
+let btn = document.getElementById('openModal');
+let closeBtn = document.querySelector('.close-modal');
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
